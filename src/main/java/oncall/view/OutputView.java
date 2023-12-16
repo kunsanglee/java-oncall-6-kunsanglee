579dd15b06f3caa6c641a8a02e47d079e93145ae
kunsanglee <gosmdochee2@gmail.com>
2023-12-16 16:31:12 +0900

    feat: 근무 순번을 형식에 맞게 출력하는 기능 구현

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,4 +1,35 @@
 package oncall.view;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import oncall.domain.Day;
+import oncall.domain.EmergencyWorker;
+import oncall.domain.Holiday;
+
 public class OutputView {
+    private static final String HOLIDAY = "(휴일)";
+    private static final String DATE_FORMAT = "%d월 %d일 %s %s";
+
+    public void printEmergencyWork(List<EmergencyWorker> emergencyWorkers) {
+        emergencyWorkers.forEach(worker -> {
+            LocalDate localDate = worker.getLocalDate();
+            DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+            String name = worker.getName();
+            if (!(dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) && Holiday.isHoliday(localDate)) {
+                printHolidayForm(localDate, Day.of(localDate.getDayOfWeek()) + HOLIDAY, name);
+            }
+            printDefaultForm(localDate, name);
+        });
+    }
+
+    private static void printHolidayForm(LocalDate localDate, String localDate1, String name) {
+        System.out.println(
+                String.format(DATE_FORMAT, localDate.getMonth().getValue(), localDate.getDayOfMonth(),
+                        localDate1, name));
+    }
+
+    private static void printDefaultForm(LocalDate localDate, String name) {
+        printHolidayForm(localDate, Day.of(localDate.getDayOfWeek()), name);
+    }
 }
