7e627c13a5a5dd5047ba3fd93b5f3dba2e13ba9a
kunsanglee <gosmdochee2@gmail.com>
2023-12-16 16:58:02 +0900

    refactor: 코드 리팩토링 - 입력 검증에 실패할 경우 재처리하는 로직을 Retry클래스로 분리하여 EmergencyWorkPlanner에서 사용하도록 수정

--- src/main/java/oncall/EmergencyWorkPlanner.java
+++ src/main/java/oncall/EmergencyWorkPlanner.java
@@ -1,9 +1,9 @@
 package oncall;
 
-import java.time.DayOfWeek;
+import static oncall.util.Retry.retry;
+
 import java.time.LocalDate;
 import java.util.List;
-import java.util.function.Supplier;
 import oncall.domain.EmergencyWorker;
 import oncall.domain.Worker;
 import oncall.view.InputView;
@@ -19,22 +19,9 @@ public class EmergencyWorkPlanner {
     }
 
     public void run() {
-        LocalDate localDate = retry(() -> inputView.readEmergencyWorkMonthDay());
-        Worker worker = retry(() -> inputView.readWorker());
-        // 입력받은 월, 요일로 시작해서 근무표를 짜야됨.
-        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        LocalDate localDate = retry(inputView::readEmergencyWorkMonthDay);
+        Worker worker = retry(inputView::readWorker);
         List<EmergencyWorker> emergencyWorkers = worker.makeEmergencyWork(localDate);
-
         outputView.printEmergencyWork(emergencyWorkers);
-    }
-
-    public static <T> T retry(Supplier<T> supplier) {
-        while (true) {
-            try {
-                return supplier.get();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
     }
 }
